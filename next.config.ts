5dba1dd5215a403fccf469cfb5e8dd8d65d4d7c6
BAMP-code <110781576+BAMP-code@users.noreply.github.com>
2026-03-10 00:53:59 -0700

    fixing dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {},
+  transpilePackages: [
+    "@deck.gl/core",
+    "@deck.gl/layers",
+    "@deck.gl/mapbox",
+    "@luma.gl/core",
+    "@luma.gl/webgl",
+    "@luma.gl/shadertools",
+    "@math.gl/core",
+    "@math.gl/web-mercator",
+  ],
 };
 
 export default nextConfig;
